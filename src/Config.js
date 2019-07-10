f9c40512876b341a65dd2d89fa768c0603b7e210
EnnyHellsen <enny.hellsen.j@gmail.com>
2019-07-10 17:27:15 +0200

    acces token

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,7 @@
 export default {
-    instagram: {
-                name: "instagram",
-                endpoint: "https://api.instagram.com/v1/users/self/",
-                region: config.apiGateway.REGION
-
-    }
+  instagram: {
+    name: "instagram",
+    endpoint: "https://api.instagram.com/v1/users/self/",
+    accessToken: "7165710768.1677ed0.0d9aa78c95374dfebe4da25a6690f899"
+  }
 };
