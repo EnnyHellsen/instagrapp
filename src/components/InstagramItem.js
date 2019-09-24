ceac1304c6f5d0bfc315540a7ef397ab86b7a7ac
EnnyHellsen <enny.hellsen@weahead.se>
2019-09-24 19:34:07 +0200

    refactor rendering instagram images

--- src/components/InstagramItem.js
+++ src/components/InstagramItem.js
@@ -1,5 +1,36 @@
 import React from "react";
+import './InstagramItem.css';
 
-export default ({ item, children, ...props }) => {
-  return <div className="instagram-item" {...props}>{children}</div>;
+const InstagramItem = ({ data }) => {
+
+  return (
+    <div className="instagram-container">
+      {data.map(item => {
+
+        let instagramText = item.caption.text;
+        let position = instagramText.search(/#/i);
+        position > 150 ? position = 150 : null;
+        let finalString = instagramText.slice(0, position);
+
+        if (item.tags.indexOf("norahellsen") > -1) {
+          return (
+            <div
+              className="instagram-item"
+              key={item.id}>
+              <img
+                className="instagram-image"
+                alt={item.caption.text.substr(0, 40)}
+                src={item.images.standard_resolution.url}
+              />
+              <p> {finalString.length > 149 ? finalString + `..` : finalString}</p>
+            </div>
+          )
+        } else {
+          return false;
+        }
+      })}
+    </div>
+  )
 };
+
+export default InstagramItem;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,7 +3,6 @@ import "./Home.css";
 import { API } from "aws-amplify";
 import config from "../config";
 import response from "../response";
-import { Col, Row } from "react-bootstrap";
 import InstagramItem from "../components/InstagramItem";
 import Hero from "../components/Hero";
 
@@ -18,46 +17,6 @@ export default class Home extends Component {
     this.getInstagram();
   }
 
-  renderInstagram = () => {
-    return this.state.response.data.map((value, index) => {
-
-      let instagramText = value.caption.text;
-      let position = instagramText.search(/#/i);
-      position > 150 ? position = 150 : null;
-      let finalString = instagramText.slice(0, position);
-
-      if (value.tags.indexOf("norahellsen") > -1) {
-        return (
-          <InstagramItem key={value.id}>
-            <img className="instagram-image"
-              onClick={this.popupImage}
-              id={index}
-              alt={value.caption.text.substr(0, 40)}
-              src={value.images.standard_resolution.url}
-            />
-            <p> {finalString.length > 149 ? finalString + `..` : finalString}</p>
-          </InstagramItem >
-        )
-      } else {
-        return false
-      }
-    });
-  };
-
-  popupImage = event => {
-    event.preventDefault();
-    console.log(event.target);
-    event.target.src =
-      event.target.src ===
-        this.state.response.data[event.target.id].images.standard_resolution.url
-        ? this.state.response.data[event.target.id].images.low_resolution.url
-        : this.state.response.data[event.target.id].images.standard_resolution
-          .url;
-    return console.log(
-      this.state.response.data[event.target.id].images.standard_resolution.url
-    );
-  };
-
   getInstagram = async () => {
     this.setState({ isLoading: true });
 
@@ -66,29 +25,37 @@ export default class Home extends Component {
         queryStringParameters: { access_token: config.instagram.accessToken }
       });
       this.setState({ isLoading: false, response: response });
-      console.log(response);
     } catch (e) {
-      console.log(e);
-      console.log(e.message);
-      console.log(e.body);
       this.setState({ isLoading: false });
     }
-  };
+  }
 
   render() {
-
-    let image = this.state.response.data[18].images.standard_resolution.url;
-
     return (
       <React.Fragment>
-        <Hero image={image} />
+        <Hero />
         <div className="Home">
           <div className="lander">
             {this.state.isLoading && <h1>Loading content..</h1>}
-            {!this.state.isLoading && this.renderInstagram()}
+            {!this.state.isLoading && <InstagramItem data={this.state.response.data} />}
           </div>
         </div>
       </React.Fragment>
     );
   }
 }
+
+
+// popupImage = event => {
+//   event.preventDefault();
+//   console.log(event.target);
+//   event.target.src =
+//     event.target.src ===
+//       this.state.response.data[event.target.id].images.standard_resolution.url
+//       ? this.state.response.data[event.target.id].images.low_resolution.url
+//       : this.state.response.data[event.target.id].images.standard_resolution
+//         .url;
+//   return console.log(
+//     this.state.response.data[event.target.id].images.standard_resolution.url
+//   );
+// };
