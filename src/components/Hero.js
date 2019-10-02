517e5eda380c07e374c0f5fd9c13e93c58b197f5
EnnyHellsen <enny.hellsen@weahead.se>
2019-10-02 16:56:52 +0200

    updated style and content for hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Hero.css";
 import chevron from './chevron.svg';
+import logo from '../containers/common/nora_logga.png';
 
 const Hero = ({ activePos }) => {
 
   return (
     <div className="hero">
+      <img className="hero-logo" src={logo} />
       <h1 className={activePos ? 'active' : ''}> Nora Hellsén Jewellery </h1>
+      <p> intro text hello hello this my jam jag är silversmed och jag är coolast i stan för mina smycken de äger verkligen</p>
       <a href="/#start">
-        <img src={chevron} />
+        <img className="chevron" src={chevron} />
       </a>
     </div>
   )
