cee7f1badae9ecc37cde27a278eff589fc4b5659
EnnyHellsen <enny.hellsen@weahead.se>
2019-10-11 16:06:59 +0200

    contact page fixer upper

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,20 +1,25 @@
 import React, { Component } from "react";
-import "./Home.css";
+import "./Contact.css";
 
-export default class Test extends Component {
-    constructor(props) {
-        super(props);
+export default class Contact extends Component {
+  constructor(props) {
+    super(props);
 
-    }
+  }
 
-    render() {
-        return (
-            <div className="contact">
-                <h1> Kontaktsida </h1>
-                <p> @norahellsenjewellery </p>
-                <p> nora.h-j@live.se </p>
-                <p> +46738 326 732 </p>
-            </div>
-        );
-    };
+  render() {
+    return (
+      <div className="contact">
+        <div className="contact-text-box">
+          <h1> Kontakta mig </h1>
+          <p> Vid frågor, förfrågningar eller om du vill skicka fanmail kontakta mig genom dessa medier. Jag gör allt inom silversmycken, kan även göra guldsmycken på beställning bara för att jag är så bra. Om du är intressead av det jag har gör, skriv! </p>
+        </div>
+        <div className="contact-info">
+          <p> @norahellsenjewellery </p>
+          <p> nora.h-j@live.se </p>
+          <p> +46738 326 732 </p>
+        </div>
+      </div>
+    );
+  };
 }
