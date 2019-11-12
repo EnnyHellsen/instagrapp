52062744e04962715f76a9905dafe628e3c7e015
EnnyHellsen <enny.hellsen@weahead.se>
2019-11-12 13:34:00 +0100

    added routes for dynamic pages

--- src/Routes.js
+++ src/Routes.js
@@ -3,11 +3,15 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Contact from "./containers/Contact";
 import About from "./containers/About";
+import Image from "./containers/Image";
 
-export default () => (
-  <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/kontakt" exact component={Contact} />
-    <Route path="/om-mig" exact component={About} />
-  </Switch>
-);
+export default () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/kontakt" exact component={Contact} />
+      <Route path="/om-mig" exact component={About} />
+      <Route path="/portfolio/:slug" component={Image} />
+    </Switch>
+  )
+};
--- src/components/InstagramItem/InstagramItem.js
+++ src/components/InstagramItem/InstagramItem.js
@@ -2,7 +2,6 @@ import React from "react";
 import './InstagramItem.css';
 
 const InstagramItem = ({ data, isDoneFetchingData }) => {
-
   return (
     <div className="instagram-container" id="start">
       {data.map(item => {
@@ -14,18 +13,20 @@ const InstagramItem = ({ data, isDoneFetchingData }) => {
 
         if (item.tags.indexOf("norahellsen") > -1) {
           return (
-            <div
-              className="instagram-item"
-              key={item.id}>
-              <img
-                className="instagram-image"
-                alt={item.caption.text.substr(0, 40)}
-                src={item.images.standard_resolution.url}
-              />
-              <div className="instagram-text-container">
-                <p> {finalString.length > 149 ? finalString + `..` : finalString}</p>
+            <a href={`/portfolio/${finalString.slice(0, 10)}`}>
+              <div
+                className="instagram-item"
+                key={item.id}>
+                <img
+                  className="instagram-image"
+                  alt={item.caption.text.substr(0, 40)}
+                  src={item.images.standard_resolution.url}
+                />
+                <div className="instagram-text-container">
+                  <p> {finalString.length > 149 ? finalString + `..` : finalString}</p>
+                </div>
               </div>
-            </div>
+            </a>
           )
         } else {
           return false;
