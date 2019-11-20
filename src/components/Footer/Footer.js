8b4408a2890e1246a6d92e0b5a82a8ac9912b2c1
EnnyHellsen <enny.hellsen@weahead.se>
2019-11-20 22:32:21 +0100

    new style for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,16 +5,21 @@ const Footer = ({ ...props }) => {
 
     return (
         <div className="footer" {...props}>
-            <div className="social">
-                <p> Följ mig på sociala medier </p>
+            <div className="footer-social">
                 <a href="https://www.instagram.com/norahellsenjewellery/">
                     <img alt="instagram-logo" className="instagram-logo" src="https://image.flaticon.com/icons/png/512/87/87390.png" />
                 </a>
+                <a href="https://www.instagram.com/norahellsenjewellery/">
+                    <img alt="instagram-logo" className="instagram-logo" src="https://image.flaticon.com/icons/svg/59/59439.svg" />
+                </a>
+                <p> Följ mig på sociala medier </p>
             </div>
 
-            <h3> Nora Hellsén Jewellery </h3>
-            <p> Din silversmed för alla lägen. Om du är intresserad av ett föremål på min portfolio så tveka inte att höra av dig. Jag gör specialbeställningar till smycken, assesoarer och korpus <a href="/kontakt"> Kontaka mig här </a></p>
-            <p className="second-footer"> Website made with ♥ by Enny</p>
+            <div className="footer-main">
+                <h3> Nora Hellsén Jewellery </h3>
+                <p> Din silversmed för alla lägen. Om du är intresserad av ett föremål på min portfolio så tveka inte att höra av dig. Jag gör specialbeställningar till smycken, assesoarer och korpus <a href="/kontakt"> Kontaka mig här </a></p>
+                <p className="second-footer"> Website made with ♥ by Enny</p>
+            </div>
         </div>
     )
 };
