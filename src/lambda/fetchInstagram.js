cc82e922b91a03173cbf2bb12bd683de2a5d16a0
EnnyHellsen <enny.hellsen@weahead.se>
2020-12-05 12:05:41 +0100

    refactor: remove unneccesary code

--- src/lambda/fetchInstagram.js
+++ src/lambda/fetchInstagram.js
@@ -4,18 +4,15 @@ require('dotenv').config();
 const token = process.env.INSTAGRAM_ACCESS_TOKEN
 
 exports.handler = function (event, context, callback) {
-  // your server-side functionality
 
   axios.get(`https://graph.instagram.com/me/media?fields=id,caption,media_url&access_token=${token}`)
     .then(response => {
       return response
     })
     .then(r => {
-      // console.log(r.data.data)
       callback(null, {
         statusCode: 200,
         body: JSON.stringify(r.data.data)
-        // body: JSON.stringify(r)
       });
     })
     .catch(error => {
