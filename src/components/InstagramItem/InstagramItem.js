f33157233b668b6ac6d4ea20f735f0cfe5ae21b8
EnnyHellsen <enny.hellsen@weahead.se>
2020-12-13 19:45:42 +0100

    fetch more items and sort out the ones with a portfolio tag

--- src/components/InstagramItem/InstagramItem.js
+++ src/components/InstagramItem/InstagramItem.js
@@ -10,27 +10,29 @@ const InstagramItem = ({ data, isDoneFetchingData }) => {
         position > 150 ? position = 150 : null;
         let finalString = instagramText.slice(0, position);
 
-        // if (item.tags.indexOf("norahellsen") > -1) {
-        return (
-          <a
-            href={`/portfolio/${finalString.slice(0, 10)}`}
-            key={item.id}
-          >
-            <div className="instagram-item">
-              <img
-                className="instagram-image"
-                alt={item.caption.substr(0, 40)}
-                src={item.media_url}
-              />
-              <div className="instagram-text-container">
-                <p> {finalString.length > 149 ? finalString + `..` : finalString}</p>
+        const isPortfolioItem = item.caption.match(/#norahellsen/)
+
+        if (isPortfolioItem) {
+          return (
+            <a
+              href={`/portfolio/${finalString.slice(0, 10)}`}
+              key={item.id}
+            >
+              <div className="instagram-item">
+                <img
+                  className="instagram-image"
+                  alt={item.caption.substr(0, 40)}
+                  src={item.media_url}
+                />
+                <div className="instagram-text-container">
+                  <p> {finalString.length > 149 ? finalString + `..` : finalString}</p>
+                </div>
               </div>
-            </div>
-          </a>
-        )
-        // } else {
-        //   return false;
-        // }
+            </a>
+          )
+        } else {
+          return false;
+        }
       })}
 
       {/* {
