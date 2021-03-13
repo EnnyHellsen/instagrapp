2f10d472e248ef28fd2f261ddfa0d0f34be9714d
EnnyHellsen <enny.hellsen@weahead.se>
2021-03-13 15:14:45 +0100

    fix broken active navigation menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,21 +2,22 @@ import { Link } from "react-router-dom";
 import React from "react";
 import "./Header.css";
 
-const Header = ({ extra, ...props }) => {
+const Header = () => {
 
-  const pathname = typeof window !== 'undefined' && window.location.pathname;
+  const path = typeof window !== 'undefined' ? window.location.pathname : '/';
   const [active, setActive] = React.useState('/')
 
   React.useEffect(() => {
-    setActive(pathname)
-  }, [pathname]);
+    setActive(path)
+  }, [path]);
+
 
   return (
     <React.Fragment>
       <div className="header-border"></div>
-      <div className="header" {...props}>
+      <div className="header">
         <Link className={active === '/om-mig' ? 'active-nav-item' : ''} to="/om-mig"> Om mig </Link>
-        <Link className={active === '/' ? 'active-nav-item' : ''} to="/"> Portfolio </Link>
+        <Link className={active === '/' || active.match(/portfolio/) ? 'active-nav-item' : ''} to="/"> Portfolio </Link>
         <Link className={active === '/kontakt' ? 'active-nav-item' : ''} to="/kontakt"> Kontakt </Link>
       </div>
     </React.Fragment>
