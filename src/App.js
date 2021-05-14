587dbc89e7ae67f3adb00c1f4360d66aa2b3bee4
EnnyHellsen <enny.hellsen@weahead.se>
2021-05-14 17:38:10 +0200

    general css fixes and added color variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Routes from "./Routes";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './general.css';
 
 ReactDOM.render(
     <Router>
